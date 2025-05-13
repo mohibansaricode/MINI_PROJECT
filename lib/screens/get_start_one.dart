1459ae5ffae5eab1b9780d71330183498768c906
mohibansaricode <mmcode46@gmail.com>
2025-05-13 22:10:59 +0500

    mini project 1st Screen complete

--- lib/screens/get_start_one.dart
+++ lib/screens/get_start_one.dart
@@ -9,10 +9,10 @@ class GetStartScreenOne extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            margin: EdgeInsets.only(top: 33, left: 42),
-            padding: EdgeInsets.only(top: 15),
+            margin: EdgeInsets.only(top: 33, left: 20),
+            padding: EdgeInsets.only(top: 29),
             width: 294,
-            height: 298,
+            height: 269,
             // color: Colors.amber,
             child: Column(
               children: [
@@ -20,8 +20,7 @@ class GetStartScreenOne extends StatelessWidget {
                   """
 Your holiday
 shopping
-delivered to Screen one    🏠
-            """,
+delivered to Screen one    🏠""",
                   style: TextStyle(
                     fontSize: 30,
                     fontFamily: "Manrope",
@@ -30,7 +29,7 @@ delivered to Screen one    🏠
                   ),
                 ),
                 Container(
-                  width: 260,
+                  width: 272,
                   height: 66,
                   // color: Colors.blue,
                   margin: EdgeInsets.only(right: 33),
@@ -38,8 +37,7 @@ delivered to Screen one    🏠
                     """
 There's something for everyone
 to enjoy with Sweet Shop
-Favourites Screen 1
-""",
+Favourites Screen 1 """,
                     style: TextStyle(
                       fontSize: 16,
                       fontFamily: "Manrope",
@@ -49,6 +47,71 @@ Favourites Screen 1
                   ),
                 ),
               ],
+            ),
+          ),
+          Container(
+            // color: Colors.white,
+            child: Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  height: 3,
+                  width: 24,
+                  margin: EdgeInsets.only(top: 35, left: 42),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(248, 249, 251, 1),
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  height: 3,
+                  width: 34,
+                  margin: EdgeInsets.only(top: 35, left: 8),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 129,
+            height: 129,
+            margin: EdgeInsets.only(top: 100),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage("assets/images/screen_one_image.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 80),
+            width: 253,
+            height: 70,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromRGBO(
+                  250,
+                  251,
+                  253,
+                  1,
+                ), // Button کا رنگ
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), // 🔥 Border Radius
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                "Get Started           →",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: "Manrope",
+                  fontWeight: FontWeight.w600,
+                  color: Color.fromRGBO(30, 34, 43, 1),
+                ),
+              ),
             ),
           ),
         ],
