cd8dad465ebbfce9b04d4a17a8161a85555157b3
mohibansaricode <mmcode46@gmail.com>
2025-05-19 23:31:14 +0500

    mini project category contanier

--- lib/screens/fourth_screen.dart
+++ lib/screens/fourth_screen.dart
@@ -1,3 +1,182 @@
+// import 'package:flutter/material.dart';
+// import 'package:mini_project/widgets/custom_widgets.dart';
+
+// class FourthScreen extends StatefulWidget {
+//   const FourthScreen({super.key});
+
+//   @override
+//   State<FourthScreen> createState() => _FourthScreenState();
+// }
+
+// class _FourthScreenState extends State<FourthScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: LayoutBuilder(
+//         builder: (context, constraints) {
+//           // Calculate scaleFactor (e.g., based on screen width)
+//           final scaleFactor =
+//               constraints.maxWidth / 375; // 375 = base design width
+
+//           return Column(
+//             children: [
+//               Stack(
+//                 children: [
+//                   Container(
+//                     width: MediaQuery.of(context).size.width,
+//                     height: MediaQuery.of(context).size.height * 0.32,
+//                     color: const Color.fromRGBO(42, 75, 160, 1),
+//                   ),
+//                   GroupOne(),
+//                   //Search icon
+//                   Positioned(
+//                     top: 68 * scaleFactor,
+//                     left: 280 * scaleFactor,
+//                     child: GestureDetector(
+//                       onTap: () {
+//                         setState(() {});
+//                       },
+//                       child: Container(
+//                         width: 18 * scaleFactor,
+//                         height: 18 * scaleFactor,
+//                         // color: Colors.amber,
+//                         child: Image.asset(
+//                           "assets/images/icon_images/Search.png",
+//                           color: Colors.white,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+
+//                   //TEXT container
+//                   Positioned(
+//                     top: 98 * scaleFactor,
+//                     left: 20 * scaleFactor,
+//                     child: Container(
+//                       width: 350 * scaleFactor,
+//                       height: 148 * scaleFactor,
+//                       // color: Colors.black54,
+//                       child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.start,
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Container(
+//                             child: Text(
+//                               "Shop",
+//                               style: TextStyle(
+//                                 fontSize: 50,
+//                                 fontFamily: "Manrope",
+//                                 fontWeight: FontWeight.w100,
+//                                 color: Color.fromRGBO(250, 251, 253, 1),
+//                               ),
+//                             ),
+//                           ),
+//                           Container(
+//                             child: Text(
+//                               "By Category",
+//                               style: TextStyle(
+//                                 fontSize: 50,
+//                                 fontFamily: "Manrope",
+//                                 fontWeight: FontWeight.w900,
+//                                 color: Color.fromRGBO(250, 251, 253, 1),
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               Expanded(child: Container()),
+//             ],
+//           );
+
+//           // return Stack(
+//           //   //Background Containers
+//           //   children: [
+//           //     // Background Containers
+//           //     Container(
+//           //       width: double.infinity,
+//           //       height: MediaQuery.of(context).size.height,
+//           //       color: Color.fromRGBO(248, 247, 251, 1),
+//           //     ),
+//           //     Container(
+//           //       width: double.infinity,
+//           //       height: MediaQuery.of(context).size.height * 0.34, // Scaled
+//           //       color: const Color.fromRGBO(42, 75, 160, 1),
+//           //       child: Column(),
+//           //     ),
+
+//           //     GroupOne(),
+//           //     //Search icon
+//           //     Positioned(
+//           //       top: 68 * scaleFactor,
+//           //       left: 280 * scaleFactor,
+//           //       child: GestureDetector(
+//           //         onTap: () {
+//           //           setState(() {});
+//           //         },
+//           //         child: Container(
+//           //           width: 18 * scaleFactor,
+//           //           height: 18 * scaleFactor,
+//           //           // color: Colors.amber,
+//           //           child: Image.asset(
+//           //             "assets/images/icon_images/Search.png",
+//           //             color: Colors.white,
+//           //           ),
+//           //         ),
+//           //       ),
+//           //     ),
+
+//           //     //TEXT container
+//           //     Positioned(
+//           //       top: 98 * scaleFactor,
+//           //       left: 20 * scaleFactor,
+//           //       child: Container(
+//           //         width: 350 * scaleFactor,
+//           //         height: 148 * scaleFactor,
+//           //         // color: Colors.black54,
+//           //         child: Column(
+//           //           mainAxisAlignment: MainAxisAlignment.start,
+//           //           crossAxisAlignment: CrossAxisAlignment.start,
+//           //           children: [
+//           //             Container(
+//           //               child: Text(
+//           //                 "Shop",
+//           //                 style: TextStyle(
+//           //                   fontSize: 50,
+//           //                   fontFamily: "Manrope",
+//           //                   fontWeight: FontWeight.w100,
+//           //                   color: Color.fromRGBO(250, 251, 253, 1),
+//           //                 ),
+//           //               ),
+//           //             ),
+//           //             Container(
+//           //               child: Text(
+//           //                 "By Category",
+//           //                 style: TextStyle(
+//           //                   fontSize: 50,
+//           //                   fontFamily: "Manrope",
+//           //                   fontWeight: FontWeight.w900,
+//           //                   color: Color.fromRGBO(250, 251, 253, 1),
+//           //                 ),
+//           //               ),
+//           //             ),
+//           //           ],
+//           //         ),
+//           //       ),
+//           //     ),
+
+//           //     //category container
+//           //   ],
+//           // );
+//         },
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:mini_project/widgets/custom_widgets.dart';
 
@@ -11,102 +190,93 @@ class FourthScreen extends StatefulWidget {
 class _FourthScreenState extends State<FourthScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final scaleFactor = screenWidth / 375;
+
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          // Calculate scaleFactor (e.g., based on screen width)
-          final scaleFactor =
-              constraints.maxWidth / 375; // 375 = base design width
+      body: Column(
+        children: [
+          // 🔷 Top section
+          Container(
+            width: screenWidth,
+            height: screenHeight * 0.32,
+            color: const Color.fromRGBO(42, 75, 160, 1),
+            child: Stack(
+              children: [
+                // 🟣 GroupOne widget (wrap in Positioned if needed)
+                Align(alignment: Alignment.topCenter, child: GroupOne()),
 
-          return Stack(
-            //Background Containers
-            children: [
-              // Background Containers
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height,
-                color: Color.fromRGBO(248, 247, 251, 1),
-              ),
-              Container(
-                width: double.infinity,
-                height: 260 * scaleFactor, // Scaled
-                color: const Color.fromRGBO(42, 75, 160, 1),
-              ),
-
-              GroupOne(),
-              //Search icon
-              Positioned(
-                top: 68 * scaleFactor,
-                left: 280 * scaleFactor,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {});
-                  },
-                  child: Container(
-                    width: 18 * scaleFactor,
-                    height: 18 * scaleFactor,
-                    // color: Colors.amber,
-                    child: Image.asset(
-                      "assets/images/icon_images/Search.png",
-                      color: Colors.white,
+                // 🔍 Search icon
+                Positioned(
+                  top: 68 * scaleFactor,
+                  left: screenWidth * 0.75,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {});
+                    },
+                    child: SizedBox(
+                      width: 18 * scaleFactor,
+                      height: 18 * scaleFactor,
+                      child: Image.asset(
+                        "assets/images/icon_images/Search.png",
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              //TEXT container
-              Positioned(
-                top: 98 * scaleFactor,
-                left: 20 * scaleFactor,
-                child: Container(
-                  width: 350 * scaleFactor,
-                  height: 148 * scaleFactor,
-                  // color: Colors.black54,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        child: Text(
+                // 📝 Texts
+                Positioned(
+                  top: 98 * scaleFactor,
+                  left: 20 * scaleFactor,
+                  child: SizedBox(
+                    width: screenWidth * 0.85,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
                           "Shop",
                           style: TextStyle(
-                            fontSize: 50,
+                            fontSize: 42 * scaleFactor,
                             fontFamily: "Manrope",
                             fontWeight: FontWeight.w100,
-                            color: Color.fromRGBO(250, 251, 253, 1),
+                            color: const Color.fromRGBO(250, 251, 253, 1),
                           ),
                         ),
-                      ),
-                      Container(
-                        child: Text(
+                        Text(
                           "By Category",
                           style: TextStyle(
-                            fontSize: 50,
+                            fontSize: 42 * scaleFactor,
                             fontFamily: "Manrope",
                             fontWeight: FontWeight.w900,
-                            color: Color.fromRGBO(250, 251, 253, 1),
+                            color: const Color.fromRGBO(250, 251, 253, 1),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              ],
+            ),
+          ),
 
-              Expanded(
+          // 🔽 Bottom section
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                color: const Color.fromRGBO(248, 247, 251, 1),
                 child: Column(
                   children: [
-                    Container(
-                      width: 327 * scaleFactor,
-                      height: 164 * scaleFactor,
-                      color: Colors.red,
-                    ),
+                    SizedBox(height: 20),
+                    Container(width: 327, height: 164, color: Colors.red),
                   ],
                 ),
               ),
-            ],
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
